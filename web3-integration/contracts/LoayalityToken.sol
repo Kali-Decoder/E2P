ff33380ea07cd417cb22fa8e61b40fe46f9ad2c8
Neeraj Choubisa <neerajchoubisa876@gmail.com>
2023-12-08 23:26:23 +0530

    Contract1: Loyality Token for Platform

---
+++ web3-integration/contracts/LoayalityToken.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+contract LoyalityToken is ERC20Burnable, Ownable {
+    constructor() ERC20("Loyal", "LOY") {
+        mint(msg.sender, 10000);
+    }
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount * (10**18));
+    }
+    
+    function burn(uint256 value) public override {
+        _burn(msg.sender, value);
+    }
+}
