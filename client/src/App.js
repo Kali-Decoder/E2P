7250fa9576d75dd47a2b8f8e28db5e097e9eb448
Snehagupta1907 <snehagupta98930@gmail.com>
2023-12-09 05:26:52 +0530

    landing updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
 import React, { Component, Fragment } from "react";
 import LandingPage from "./pages/LandingPage";
 import { Routes, Route } from "react-router-dom";
+import BackGradients from "./components/BackGradients";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" exact element={<LandingPage />} />
+        <Route path="/" exact element={
+        <>
+        <div className="-z-30 back-light transition-all">
+        <LandingPage />
+        <BackGradients page="home" />
+        </div>
+        </>} />
       </Routes>
+      
     </>
   );
 }
