e1ecaaf41429dfca2931902556a495e0697a3a6c
Snehagupta1907 <snehagupta98930@gmail.com>
2023-12-10 03:09:14 +0530

    user dash

--- client/src/pages/UserDashboard.jsx
+++ client/src/pages/UserDashboard.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Sidebar from '../components/user/Sidebar'
+import Dashboard from '../components/user/Dashboard'
+import Table from '../components/user/Table'
+
 
 const UserDashboard = () => {
   return (
-    <div>UserDashboard</div>
+   <>
+   <Sidebar/>
+   <Dashboard/>
+   </>
   )
 }
 
