2bc5ed3de429fbce74fe80f9bf67727c4397f33f
Snehagupta1907 <snehagupta98930@gmail.com>
2023-12-10 04:19:24 +0530

    update

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -87,19 +87,19 @@ const LandingPage = () => {
               <nav className="flex flex-wrap justify-center items-center mb-0 text-base  md:border-gray-200">
                 <a
                   href="#_"
-                  className="mr-4 text-2xl font-medium leading-6 text-gray-600 hover:text-white"
+                  className="mr-4 text-2xl font-bold leading-6 text-gray-400 hover:text-white"
                 >
                   Home
                 </a>
                 <a
                   href="#features"
-                  className="mr-4 text-2xl font-medium leading-6 text-gray-600 hover:text-white"
+                  className="mr-4 text-2xl font-bold leading-6 text-gray-400 hover:text-white"
                 >
                   Features
                 </a>
                 <a
                   href="#usecases"
-                  className="mr-4 text-2xl font-medium leading-6 text-gray-600 hover:text-white"
+                  className="mr-4 text-2xl font-bold leading-6 text-gray-400 hover:text-white"
                 >
                   Use-Cases
                 </a>
@@ -849,7 +849,7 @@ const LandingPage = () => {
             <div className="px-5 py-2">
               <a
                 href="#"
-                className="text-base leading-6 text-gray-500 hover:text-gray-900"
+                className="text-medium leading-6 text-gray-500 hover:text-indigo-600"
               >
                 About
               </a>
@@ -857,7 +857,7 @@ const LandingPage = () => {
             <div className="px-5 py-2">
               <a
                 href="#"
-                className="text-base leading-6 text-gray-500 hover:text-gray-900"
+                className="text-medium leading-6 text-gray-500 hover:text-indigo-600"
               >
                 Blog
               </a>
@@ -865,7 +865,7 @@ const LandingPage = () => {
             <div className="px-5 py-2">
               <a
                 href="#"
-                className="text-base leading-6 text-gray-500 hover:text-gray-900"
+                className="text-medium leading-6 text-gray-500 hover:text-indigo-600"
               >
                 Team
               </a>
@@ -873,7 +873,7 @@ const LandingPage = () => {
             <div className="px-5 py-2">
               <a
                 href="#"
-                className="text-base leading-6 text-gray-500 hover:text-gray-900"
+                className="text-medium leading-6 text-gray-500 hover:text-indigo-600"
               >
                 Pricing
               </a>
@@ -881,7 +881,7 @@ const LandingPage = () => {
             <div className="px-5 py-2">
               <a
                 href="#"
-                className="text-base leading-6 text-gray-500 hover:text-gray-900"
+                className="text-medium leading-6 text-gray-500 hover:text-indigo-600"
               >
                 Contact
               </a>
@@ -889,14 +889,14 @@ const LandingPage = () => {
             <div className="px-5 py-2">
               <a
                 href="#"
-                className="text-base leading-6 text-gray-500 hover:text-gray-900"
+                className="text-medium leading-6 text-gray-500 hover:text-indigo-600"
               >
                 Terms
               </a>
             </div>
           </nav>
           <div className="flex justify-center mt-8 space-x-6">
-            <a href="#" className="text-gray-400 hover:text-gray-500">
+            <a href="#" className="text-gray-400 hover:text-indigo-600">
               <span className="sr-only">Facebook</span>
               <svg
                 className="w-6 h-6"
@@ -911,7 +911,7 @@ const LandingPage = () => {
                 ></path>
               </svg>
             </a>
-            <a href="#" className="text-gray-400 hover:text-gray-500">
+            <a href="#" className="text-gray-400 hover:text-indigo-600">
               <span className="sr-only">Instagram</span>
               <svg
                 className="w-6 h-6"
@@ -926,7 +926,7 @@ const LandingPage = () => {
                 ></path>
               </svg>
             </a>
-            <a href="#" className="text-gray-400 hover:text-gray-500">
+            <a href="#" className="text-gray-400 hover:text-indigo-600">
               <span className="sr-only">Twitter</span>
               <svg
                 className="w-6 h-6"
@@ -937,7 +937,7 @@ const LandingPage = () => {
                 <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"></path>
               </svg>
             </a>
-            <a href="#" className="text-gray-400 hover:text-gray-500">
+            <a href="#" className="text-gray-400 hover:text-indigo-600">
               <span className="sr-only">GitHub</span>
               <svg
                 className="w-6 h-6"
@@ -952,7 +952,7 @@ const LandingPage = () => {
                 ></path>
               </svg>
             </a>
-            <a href="#" className="text-gray-400 hover:text-gray-500">
+            <a href="#" className="text-gray-400 hover:text-indigo-600">
               <span className="sr-only">Kali-Decoder</span>
               <svg
                 className="w-6 h-6"
